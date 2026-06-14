3fa91a6ea602d5917365397fc9654a9f4c42d66a
jqk <156398034+jqk512@users.noreply.github.com>
2026-06-14 20:29:35 +0800

    Integrate background settings with VisualEffectsfeat: App wires background settings state to VisualEffects and AppShell
    Added background settings management to the App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { AppShell } from './components/AppShell'
 import { ProtectedRoute } from './components/ProtectedRoute'
@@ -8,21 +9,32 @@ import { ReviewPage } from './pages/ReviewPage'
 import { SearchPage } from './pages/SearchPage'
 import { LoginPage } from './pages/LoginPage'
 import { VisualEffects } from './components/VisualEffects'
-import { AppErrorBoundary } from './components/AppErrorBoundary'
+import { loadSettings, saveSettings, type BackgroundSettings } from './lib/background-settings'
 import './App.css'
 
 function App() {
+  const [bgSettings, setBgSettings] = useState<BackgroundSettings>(loadSettings)
+
+  function updateBgSettings(changes: Partial<BackgroundSettings>) {
+    setBgSettings((prev) => {
+      const next = { ...prev, ...changes }
+      saveSettings(next)
+      return next
+    })
+  }
+
   return (
-    <AppErrorBoundary>
-      <VisualEffects />
+    <>
+      <VisualEffects settings={bgSettings} />
       <AuthProvider>
         <HashRouter>
           <Routes>
             <Route path="login" element={<LoginPage />} />
             <Route element={<ProtectedRoute />}>
-              <Route element={<AppShell />}>
+              <Route element={
+                <AppShell bgSettings={bgSettings} onBgChange={updateBgSettings} />
+              }>
                 <Route index element={<LibraryPage />} />
-                <Route path="library/all" element={<LibraryPage />} />
                 <Route path="library/bank/:bankId" element={<LibraryPage />} />
                 <Route path="library/group/:groupId" element={<LibraryPage />} />
                 <Route path="import" element={<ImportPage />} />
@@ -34,7 +46,7 @@ function App() {
           </Routes>
         </HashRouter>
       </AuthProvider>
-    </AppErrorBoundary>
+    </>
   )
 }
 
